6e6e7ffb5c7848be3a1389d92ef2802353119a0c
paravena <pablo.aravena@bazaarvoice.com>
2024-06-11 18:25:01 -0400

    more changes

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -35,15 +35,19 @@ const LayoutComponent: ForwardRefRenderFunction<HTMLDivElement, Props> = (
   },
   ref,
 ) => {
-  const classNames = clsx('h-full w-full', {
-    'flex flex-wrap gap-4': files.length > 0,
-    'flex justify-center items-center': files.length === 0,
-    'border-2 border-solid border-blue-400 rounded-md': active,
+  const classNames = clsx('flex w-full', {
+    'flex-wrap gap-4 flex-grow flex-shrink items-start': files.length > 0,
+    'justify-center items-center': files.length === 0,
   });
 
   return (
-    <div className="relative h-dvh bg-gray-400 p-3">
-      <div ref={ref} className={classNames} {...dropzoneProps}>
+    <div
+      className={clsx('relative h-dvh overflow-hidden bg-gray-400  p-3', {
+        'rounded-md border-2 border-solid border-blue-400': active,
+      })}
+      {...dropzoneProps}
+    >
+      <div ref={ref} className={classNames}>
         <Input visible={files.length === 0} onChange={onChange} />
         {files.map(f => {
           return (
@@ -63,13 +67,13 @@ const LayoutComponent: ForwardRefRenderFunction<HTMLDivElement, Props> = (
           show={files.length > 0 && active}
           as={Fragment}
           enter="transform ease-out duration-300 transition"
-          enterFrom="translate-y-2 opacity-0 sm:translate-y-0"
-          enterTo="translate-y-0 opacity-100 sm:translate-y-0"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
           leave="transition ease-in duration-100"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="absolute left-2/4 top-2/4 z-10 flex -translate-x-2/4 -translate-y-2/4 transform flex-col items-center justify-center rounded-md border-2 border-solid border-blue-400 bg-gray-300 px-10 py-8 text-sm">
+          <div className="absolute left-2/4 top-2/4 z-10 flex -translate-x-1/2 -translate-y-1/2 transform flex-col items-center justify-center rounded-md border-2 border-solid border-blue-400 bg-gray-300 px-10 py-8 text-sm">
             <ArrowUpTrayIcon className="h-8 w-8" />
             <p>Drop files to upload</p>
             <p>JPEG, PNG, HEIF, WEBP, AVIF, or SVG files</p>
